887db528d1995d03a3dff15fca66c7baab94c232
Pieter Smit <pjksmit@icloud.com>
2023-07-11 10:12:39 +0200

    Added a script that automatically buys cursors

--- cookieBot.js
+++ cookieBot.js
@@ -1,5 +1,9 @@
 function init() {
     Game.Notify("CookieBot", "Hello",1,100);
-    const clickInterval = setInterval(() => Game.ClickCookie(), 4)
+    const clickInterval = setInterval(() => Game.ClickCookie(), 20)
+    if (Game.Objects[0].getSumPrice(checkAmount) < Game.cookies) {
+        Game.Notify("CookieBot", "Buy",1,100);
+        Game.Objects[0].buy(buyAmount);
+    }
 }
 init();
