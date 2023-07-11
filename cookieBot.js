5a0496025349dad39d7c8369e9e039804cd41a47
Pieter Smit <pjksmit@icloud.com>
2023-07-11 10:45:31 +0200

    bugfix

--- cookieBot.js
+++ cookieBot.js
@@ -1,10 +1,10 @@
 function init() {
     Game.Notify("CookieBot", "Hi", 1, 100);
-    console.log(Game.Objects[0].getSumPrice(1)); 
+    console.log(Game.ObjectsById[0].getSumPrice(1)); 
     const clickInterval = setInterval(() => Game.ClickCookie(), 20);
-    if (Game.Objects[0].getSumPrice(1) < Game.cookies) {
+    if (Game.ObjectsById[0].getSumPrice(1) < Game.cookies) {
         Game.Notify("CookieBot", "Buy", 1, 100);
-        Game.Objects[0].buy(1);
+        Game.ObjectsById[0].buy(1);
     }
 }
 init();
