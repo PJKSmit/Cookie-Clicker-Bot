e70850fc632b2313a6a52b2bffc599b5e88e470f
Pieter Smit <pjksmit@icloud.com>
2023-07-13 12:05:53 +0200

    Added a Cookies per second bonus calculator for the kitten upgrades and fixed some bugs

--- cookieBot.js
+++ cookieBot.js
@@ -32,16 +32,15 @@ const icons = {
 }
 
 //Multiplier of cursor upgrades after tier three
-const multiplier = [1, 5, 10, 20, 20, 20, 20, 20, 20, 20, 20, 20];
+const cursorMultiplier = [1, 5, 10, 20, 20, 20, 20, 20, 20, 20, 20, 20];
 
 //kitten multipliers.
-const kittenMults = {
-    //Work in Progress 
-}
+const kittenMultipliers = [0, 0.1, 0.125, 0.15, 0.175, 0.2, 0.2, 0.2, 0.2, 0.2, 0.175, 0.15, 0.125, 0.115, 0.11, 0.105, 0.1, 0.05];
 
 function CalculateBestOption() {
     var fastestReturnOnInvestment = Infinity;
-    for(var i = 0; i < Game.ObjectsById.length; i++) {
+    for(var i in Game.ObjectsById) {
+        if(Game.ObjectsById[i - 1] && Game.ObjectsById[i - 1].amount == 0) break;
         building = Game.ObjectsById[i];
         var returnOnInvestement = BuildingReturnOnInvestment(building);
         if(returnOnInvestement < fastestReturnOnInvestment) {
@@ -57,8 +56,7 @@ function CalculateBestOption() {
             bestOption = upgrade;
         }
     }
-    console.log(bestOption);
-    console.log(fastestReturnOnInvestment);
+    console.log(bestOption.name + " because " + fastestReturnOnInvestment);
 }
 
 function BuyBest() {
@@ -101,7 +99,7 @@ function HandleGoldenCookies() {
 }
 
 function UpgradeReturnOnInvestment(upgrade) {
-    var bonus = upgrade.getPrice() / 100;
+    var bonus = upgrade.getPrice() / 1000;
     //Cookieupgrades
     if(upgrade.pool == "cookie") {
         bonus = Game.cookiesPs * upgrade.power / 100;
@@ -116,7 +114,7 @@ function UpgradeReturnOnInvestment(upgrade) {
                 for(i in Game.Objects) {
                     amountOfBuildings += Game.Objects[i].amount;
                 }
-                bonus = amountOfBuildings * 0.01 * multiplier[upgrade.tier - 4];
+                bonus = amountOfBuildings * 0.01 * cursorMultiplier[upgrade.tier - 4];
             }
         //Non-cursor
         } else if(upgrade.icon[0] in icons && (upgrade.icon[1] < 3 || upgrade.icon[1] > 12) && upgrade.icon[1] != 32) {
@@ -127,15 +125,26 @@ function UpgradeReturnOnInvestment(upgrade) {
             bonus = clicksPerSecond * Game.cookiesPs / 100;
         //Kittens
         } else if(upgrade.icon[0] == 18 && (upgrade.icon[1] < 3 || upgrade.icon[1] > 12) && upgrade.icon[1] != 32) {
-            //Work in progress
+            var milkMult=1;
+			if (Game.Has('Santa\'s milk and cookies')) milkMult*=1.05;
+			milkMult*=1+Game.auraMult('Breath of Milk')*0.05;
+			if (Game.hasGod)
+			{
+				var godLvl=Game.hasGod('mother');
+				if (godLvl==1) milkMult*=1.1;
+				else if (godLvl==2) milkMult*=1.05;
+				else if (godLvl==3) milkMult*=1.03;
+			}
+			milkMult*=Game.eff('milk');
+            bonus = Game.cookiesPs * (1 + Game.milkProgress * kittenMultipliers[upgrade.tier] * milkMult)
         //Grandma synergie 
         } else if(upgrade.icon = [10, 9]) {
-            const isRightBuilding = (element) => element = upgrade.buildingTie;
-            var grandmaNumber = Game.ObjectsById.findIndex(isRightBuilding) - 1;
+            var grandmaNumber = upgrade.buildingTie.id - 1;
             bonus = Game.ObjectsById[1].amount / grandmaNumber * upgrade.buildingTie.storedTotalCps / 100 + Game.ObjectsById[1].storedTotalCps;
         }
+    //Tech upgrades
     } else if(upgrade.pool == "tech") {
-
+        //Work in progress
     }
     return (upgrade.getPrice() / bonus) + (Math.max(upgrade.getPrice() - Game.cookies, 0) / (Game.cookiesPs + 10));
 }
@@ -144,8 +153,7 @@ function init() {
     if (!Game.ready) {
         setTimeout(() => init(), 1000);
         return;
-      }
-    CalculateBestOption();
+    }
     var nameCheckInterval = setInterval(() => CheckName(), 1000);
 }
 
